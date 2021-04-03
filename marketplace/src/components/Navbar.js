14681730ace2656dea8e3c0b51d0aa77a76ad4d8
Dâmaso Júnio <junio.pereirab@gmail.com>
2021-04-03 01:41:02 -0300

    Creating navbar

---
+++ marketplace/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+function NavBar() {
+  return (
+    <navbar className="mainNavBar">Marketplace</navbar>
+  );
+}
+
+export default NavBar;
